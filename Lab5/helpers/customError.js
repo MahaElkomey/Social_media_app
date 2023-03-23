a3a9a7cdd109756e57c60c829516a76982a1cbe2
Maha-Elkomey <47718954+MahaElomey@users.noreply.github.com>
2023-03-23 16:34:21 +0200

    clean up

--- Lab5/helpers/customError.js
+++
@@ -1,8 +0,0 @@
-class CustomError extends Error {
-	constructor(message,statusCode,errors){
-		super(message);
-		this.statusCode = statusCode;
-		this.errors = errors;
-	}
-}
-module.exports = CustomError
